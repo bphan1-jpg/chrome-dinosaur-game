3f116d3697e16776e898f104d7debc354388d1d8
Benjamin Phan <bphan1@ocdsb.ca>
2021-12-08 11:39:33 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,60 @@
+let unicorn;
+let uImg;
+let tImg;
+let bImg;
+let trains = [];
+
+function preload() {
+  const options = {
+    probabilityThreshold: 0.95
+  };
+
+  uImg = loadImage('unicorn.png');
+  tImg = loadImage('train.png');
+  bImg = loadImage('background.jpg');
+}
+
+function mousePressed() {
+  trains.push(new Train());
+}
+
+function setup() {
+  createCanvas(800, 450);
+  unicorn = new Unicorn();
+}
+
+function gotCommand(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results[0].label, results[0].confidence);
+  if (results[0].label == 'up') {
+    unicorn.jump();
+  }
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    unicorn.jump();
+  }
+}
+
+function draw() {
+  
+  if (random(1) < 0.005) {
+    trains.push(new Train());
+  }
+  
+  background(bImg);
+  for (let t of trains) {
+    t.move();
+    t.show();
+    if (unicorn.hits(t)) {
+      console.log('game over');
+      noLoop();
+    }
+  }
+
+  unicorn.show();
+  unicorn.move();
+}
